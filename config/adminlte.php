77ff0b494606804073914f0e5ad91ddd53912373
restu32 <mochresturespati2@gmail.com>
2021-05-22 19:03:47 +0700

    Tugas Besar PWL-A

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Rest</b>Market',
     'logo_img' => 'vendor/adminlte/dist/img/rest.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'Rest Market',
 
     /*
     |--------------------------------------------------------------------------
